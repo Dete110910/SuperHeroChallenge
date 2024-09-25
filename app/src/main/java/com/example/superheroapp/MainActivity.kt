e79d9d29dfd699c4bfce6ec1231cfcc2af0f81c7
DanTan <alexanderlml1997@gmail.com>
2024-09-24 22:31:24 -0500

    Modify Superhero and Enemy to extends of Character. Implements loading of rv with superheroes and villians

--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -1,30 +1,62 @@
 package com.example.superheroapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.superheroapp.databinding.ActivityMainBinding
 import com.example.superheroapp.ui.screens.characters.rv.RvCharacterAdapter
 import com.example.superheroapp.data.*
+import com.example.superheroapp.data.models.Character
+import com.example.superheroapp.data.viewModel.CharactersViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var rvCharactersAdapter : RvCharacterAdapter
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var rvCharactersAdapter: RvCharacterAdapter
+    private val characterViewModel: CharactersViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initRV()
+        setListeners()
+        initUiStateLifecycle()
+
+        characterViewModel.loadCharacters(isHero = true)
     }
 
     private fun initRV() {
-        rvCharactersAdapter = RvCharacterAdapter(generateSuperheroes())
+        rvCharactersAdapter = RvCharacterAdapter()
         binding.rvHeroesVillans.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = rvCharactersAdapter
         }
+    }
 
+    private fun setListeners() {
+        binding.btnSuperHero.setOnClickListener {
+            characterViewModel.loadCharacters(isHero = true)
+        }
+
+        binding.btnVillian.setOnClickListener {
+            characterViewModel.loadCharacters(isHero = false)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initUiStateLifecycle() {
+        lifecycleScope.launch {
+            characterViewModel.uiState.collect { uiState ->
+                uiState.characterList.let { characterList ->
+                    rvCharactersAdapter.characters = characterList
+                    rvCharactersAdapter.notifyDataSetChanged()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/superheroapp/ui/screens/characters/rv/RvCharacterAdapter.kt
+++ app/src/main/java/com/example/superheroapp/ui/screens/characters/rv/RvCharacterAdapter.kt
@@ -3,16 +3,16 @@ package com.example.superheroapp.ui.screens.characters.rv
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.superheroapp.data.models.Character
 import java.util.Objects
 
 import com.example.superheroapp.data.models.Superhero
 import com.example.superheroapp.databinding.ActivityCharacterBinding
 
-
-
 class RvCharacterAdapter(
-    private val characters: List<Superhero>
 ) : RecyclerView.Adapter<CharacterViewHolder>() {
+
+    var characters = emptyList<Character>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         val binding = ActivityCharacterBinding.inflate(
